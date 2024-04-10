abdfdc25299f82591c193fea3db62ec0eb4094dd
Bordiga541 <bordigagabriel1@gmail.com>
2024-04-10 12:13:04 -0300

    2222222

--- pryBordigaGabriel/Properties/Resources.Designer.cs
+++ pryBordigaGabriel/Properties/Resources.Designer.cs
@@ -59,25 +59,5 @@ namespace pryBordigaGabriel.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap firma {
-            get {
-                object obj = ResourceManager.GetObject("firma", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap fondo {
-            get {
-                object obj = ResourceManager.GetObject("fondo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
